a2ce574348bb14b453430cd4d0a9f6b443d53499
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-11-25 16:46:10 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,44 @@
+a = 10 ;
+b = 5 ;
+
+c = a+b ; // Addition operator 
+d = a*b ; // Mltipication Operator 
+e = a/b ; // Divison Operator 
+// d = a > b comparison operator
+
+console.log(c);
+console.log(d);
+console.log (e);
+
+
+let x = 50 ;
+let y = (x+30)*3
+let z = x**3; // Exponentiation
+
+let m = a % b;
+console.log(m)
+console.log(y)
+console.log(z)
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+
+console.log(text3)
+
+let texta= "What a very ";
+texta += "nice day";
+console.log(texta) //The += assignment operator can also be used to add string
+
+let sum = 50 ;
+let result= sum >= 40
+
+let p= 5;
+p++;
+let f = p;
+
+console.log(f)
+
+p -- ;
+let t = p;
+console.log(t)
