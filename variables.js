1aa3035aa21ff6a8320297a04df8b4141b781414
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-11-25 16:12:49 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,18 @@
+let a = "Omkar ";
+
+console.log(a);
+
+
+var b = 20;
+var b= 19 ;
+b = 30;
+console.log(b)
+
+
+let c = 25 ;
+c = 35 ;
+// let c =45 is not allowed 
+
+const d = 45;
+// d ="Ram " is not allowed 
+console.log(d)
