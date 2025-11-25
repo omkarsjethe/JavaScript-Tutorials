9175e85e3cdda9a259027b9f6bc49ff012784194
Omkar Jethe <98147636+omkarsjethe@users.noreply.github.com>
2025-11-25 17:24:30 +0530

    Add files via upload

---
+++ IFstatements.js
@@ -0,0 +1,21 @@
+let count = 10;
+
+if (count) { // Since 10 is a non-zero number, it's Truthy
+  console.log("Count is not zero."); // This executes
+}
+
+let age = 27 ;
+
+if (age >= 18){
+    console.log("You can ride ")
+}
+
+let marks = 23 ;
+if (marks < 35 ){
+    console.log("You are failed")
+}
+
+let name = "Omkar"
+if (name ="Omkar"){
+    console.log("Access granted")
+}
